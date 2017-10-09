377e8b043b9e69d47b495faee3c628e8d0b07580
eduardosm7 <esm7@cin.ufpe.br>
2017-10-09 22:38:27 +0000

    Deleted models

--- WhereIGo/app/models/client.rb
+++
@@ -1,7 +0,0 @@
-class Client < ApplicationRecord
-    after_initialize :set_default_values
-    
-    def set_default_values
-        self.is_active ||= true
-    end
-end
--- WhereIGo/app/models/provider.rb
+++
@@ -1,7 +0,0 @@
-class Provider < ApplicationRecord
-    after_initialize :set_default_values
-    
-    def set_default_values
-        self.is_active ||= true
-    end
-end
