1873cd4b0d1a65bfd745490af91edd03866f1f96
aacgn <aacgn@cin.ufpe.br>
2017-10-17 19:10:08 +0000

    Fix users model

--- WhereIGo/db/migrate/20171009215602_create_users.rb
+++ WhereIGo/db/migrate/20171009215602_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :name
       t.string :email
       t.string :password_digest
