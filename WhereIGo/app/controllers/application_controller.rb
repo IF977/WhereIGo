4787f5c06c6af4ccfd07207c105a8ba8749c2fce
Vinicius Giles <viniciusgcp@gmail.com>
2017-10-18 03:13:10 +0000

    Fix homepage team section

--- WhereIGo/app/controllers/application_controller.rb
+++ WhereIGo/app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def index
+      @title = "WhereIGo"
+      render layout: false
   end
   
 end
--- WhereIGo/config/routes.rb
+++ WhereIGo/config/routes.rb
@@ -36,6 +36,6 @@ Rails.application.routes.draw do
     post 'establishments/update/:id' => 'establishment#update'
 
     
-    root 'account#login' 
+    root 'application#index' 
   
 end
