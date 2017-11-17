ff0044f8c199f63f0ed636dce5a0c76e0348786e
aacgn <aacgn@cin.ufpe.br>
2017-11-17 20:12:49 +0000

    Establishment description

--- WhereIGo/db/schema.rb
+++ WhereIGo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114012744) do
+ActiveRecord::Schema.define(version: 20171117200210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20171114012744) do
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "food_prefences", force: :cascade do |t|
