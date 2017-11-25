4c78d641c0cccb8aad3bf7c66c598ee3bded440a
mvhb <mvhb@cin.ufpe.br>
2017-11-25 23:27:39 +0000

    preferences tests - rspec #72

--- WhereIGo/spec/models/ambient_preference_spec.rb
+++ WhereIGo/spec/models/ambient_preference_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe AmbientPreference, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+    before do
+        values = {:name => 'edu',
+                  :email => 'edu@edu',
+                  :password_digest => 'eduedu'
+        }
+        @user = User.create values
+        valuesAmbient = {:name => 'familiar'
+        }
+        @ambient = Ambient.create valuesAmbient
+    end
+    context "preferencia ambiente" do
+        it "preferencia valida - user id e ambient id" do
+            ambientPreferences = AmbientPreference.new(user_id: @user.id, ambient_id: @ambient.id)
+            expect(ambientPreferences.valid?).to be_truthy
+        end
+        it "preferencia invalida - sem user id" do
+            ambientPreferences = AmbientPreference.new(user_id: nil, ambient_id: @ambient.id)
+            expect(ambientPreferences.valid?).to be_falsey
+        end
+        it "preferencia valida - sem ambient id" do
+            ambientPreferences = AmbientPreference.new(user_id: @user.id, ambient_id: nil)
+            expect(ambientPreferences.valid?).to be_falsey
+        end
+    end
 end
